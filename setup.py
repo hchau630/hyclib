ef6e1079c6f93436b44162a144b6b90068a0499d
hc3190 <hc3190@ax04.rc.zi.columbia.edu>
2022-02-17 03:23:28 -0500

    Updated version to 0.2.0 and modified requirements.txt

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hyc-utils',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     extras_require={
         'packaging': ['twine'],
