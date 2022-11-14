d7510b522a822e65c29e963db1894a5a7b142191
Ho Yin Chau <hoyinchau@dyn-160-39-161-158.dyn.columbia.edu>
2022-11-14 15:51:22 -0500

    version 0.5.4: Added regplot and changed some package stuff

--- setup.py
+++ setup.py
@@ -2,15 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='hyc-utils',
-    version='0.5.3',
+    version='0.5.4',
     packages=find_packages(),
     install_requires=[
         'numpy',
         'matplotlib',
+        'statsmodels',
         'torch',
         'tomli',
     ],
-    extras_require={
-        'dev': ['pandas','scipy','pytest','twine'],
-    }
 )
