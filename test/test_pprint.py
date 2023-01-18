cbf5db6c651d07ac0b8e43d719724cd896414705
Ho Yin Chau <hoyinchau@Hos-MacBook-Pro-2.local>
2023-01-18 15:46:28 -0500

    0.5.10 - added verbose option to pprint

--- test/test_pprint.py
+++ test/test_pprint.py
@@ -6,4 +6,4 @@ import utils
 def test_pformat():
     d = {'a': torch.Tensor([[1.0,2.0],[3.0,4.0]]), 'b': {'c': np.array([[1.0,2.0],[3.0,4.0]]), 'd': 10.0}}
     expected = "'a':\n    tensor([[1., 2.],\n            [3., 4.]])\n'b':\n    'c':\n        [[1. 2.]\n         [3. 4.]]\n    'd':\n        10.0"
-    assert utils.pprint.pformat(d) == expected
+    assert utils.pprint.pformat(d, verbose=True) == expected
