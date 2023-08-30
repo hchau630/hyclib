21b0c0fa3b579081c739d281381c0b47d510af18
Ho Yin Chau <hoyinchau@Hos-MacBook-Pro-2.local>
2023-08-30 12:42:47 -0400

    Added rpartial to functools and added test for it.

--- src/hyclib/core/functools.py
+++ src/hyclib/core/functools.py
@@ -1,7 +1,73 @@
+import reprlib
 import functools
 import warnings
 
-from . import pprint
+class rpartial:
+    """New function with partial application of the given arguments
+    and keywords, but the partial args are appended to the function 
+    args rather than prepended as in functools.partial.
+    
+    Implementation adapted from the cpython 3.11 source code for
+    functools.partial.
+    """
+
+    __slots__ = "func", "args", "keywords", "__dict__", "__weakref__"
+
+    def __new__(cls, func, /, *args, **keywords):
+        if not callable(func):
+            raise TypeError("the first argument must be callable")
+
+        if hasattr(func, "func"):
+            args = args + func.args
+            keywords = {**func.keywords, **keywords}
+            func = func.func
+
+        self = super(rpartial, cls).__new__(cls)
+
+        self.func = func
+        self.args = args
+        self.keywords = keywords
+        return self
+
+    def __call__(self, /, *args, **keywords):
+        keywords = {**self.keywords, **keywords}
+        return self.func(*args, *self.args, **keywords)
+
+    @reprlib.recursive_repr()
+    def __repr__(self):
+        qualname = type(self).__qualname__
+        args = [repr(self.func)]
+        args.extend(repr(x) for x in self.args)
+        args.extend(f"{k}={v!r}" for (k, v) in self.keywords.items())
+        return f"{qualname}({', '.join(args)})"
+
+    def __reduce__(self):
+        return type(self), (self.func,), (self.func, self.args,
+               self.keywords or None, self.__dict__ or None)
+
+    def __setstate__(self, state):
+        if not isinstance(state, tuple):
+            raise TypeError("argument to __setstate__ must be a tuple")
+        if len(state) != 4:
+            raise TypeError(f"expected 4 items in state, got {len(state)}")
+        func, args, kwds, namespace = state
+        if (not callable(func) or not isinstance(args, tuple) or
+           (kwds is not None and not isinstance(kwds, dict)) or
+           (namespace is not None and not isinstance(namespace, dict))):
+            raise TypeError("invalid partial state")
+
+        args = tuple(args) # just in case it's a subclass
+        if kwds is None:
+            kwds = {}
+        elif type(kwds) is not dict: # XXX does it need to be *exactly* dict?
+            kwds = dict(kwds)
+        if namespace is None:
+            namespace = {}
+
+        self.__dict__ = namespace
+        self.func = func
+        self.args = args
+        self.keywords = kwds
 
 def parametrized(dec):
     """
